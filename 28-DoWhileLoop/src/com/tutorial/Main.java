74d33f8c4ed0be66b59dbb8f4c9c6496f4d815b4
Dzikr11 <english9.ghulaam@gmail.com>
2019-12-21 11:26:27 +0700

    A2.1900054-DzikriGhulaamZakiyya

---
+++ 28-DoWhileLoop/src/com/tutorial/Main.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * @author Windows_10
+ * Nama     :Dzikri Ghulaam Zakiyya
+ * NIM      :A2.1900054
+ * Kelas    :TI-1C
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //        do {
+//            aksi
+//        } while (kondisi);
+
+
+        System.out.println("ini adalah awal program");
+
+        int a = 0;
+        boolean kondisi = true;
+
+
+        do {
+            a++;
+            System.out.println("do while ke-" + a);
+
+            if (a == 1) {
+                kondisi = false;
+            }
+
+        }while(kondisi);
+
+        System.out.println("ini adalah akhir program");
+    }
+    
+}
